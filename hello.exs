1d9ac2cdbbe62ed7253ef59c8864321e42402ddf
blck-snwmn <whitesnowmancreator@gmail.com>
2022-09-28 01:10:03 +0900

    Format by elixir-ls

--- hello.exs
+++ hello.exs
@@ -1,18 +1,26 @@
-IO.puts "Hello world from Elixir"
-l = ['a','b','c','d']
+IO.puts("Hello world from Elixir")
+l = ['a', 'b', 'c', 'd']
 [h | t] = l
-IO.puts h
-IO.puts t
-[a,b,c] = t
-IO.puts a
-IO.puts b
-IO.puts c
+IO.puts(h)
+IO.puts(t)
+[a, b, c] = t
+IO.puts(a)
+IO.puts(b)
+IO.puts(c)
+
 x = fn
-    x, y when x > 10 -> x+y*10
-    x, y -> x+y
-  end
-IO.puts x.(10 ,20)
-IO.puts x.(11 ,20)
-IO.inspect <<0::1, 0::1, 1::1, 1::1>>
-IO.inspect <<3::4>>
-IO.puts <<0::1, 0::1, 1::1, 1::1>> == <<3::4>>
+  x, y when x > 10 -> x + y * 10
+  x, y -> x + y
+end
+
+IO.puts(x.(10, 20))
+IO.puts(x.(11, 20))
+IO.inspect(<<0::1, 0::1, 1::1, 1::1>>)
+IO.inspect(<<3::4>>)
+IO.puts(<<0::1, 0::1, 1::1, 1::1>> == <<3::4>>)
+IO.inspect(String.split("1 2  3", " ", trim: true))
+
+kl = [a: 2, b: "qaaa"] ++ [c: true]
+IO.puts(kl[:a])
+IO.puts(kl[:b])
+IO.puts(kl[:c])
